3eff3f7e4c82f1608f4f2fa5227fb5c5780a9a04
Max <maxvhhg@gmail.com>
2023-05-28 14:05:06 +0300

    Написан первый графический интерфейс

---
+++ app/src/main/java/com/vhhg/airport/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.vhhg.airport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Button register = findViewById(R.id.register);
+        Button login = findViewById(R.id.login);
+        Button woregister = findViewById(R.id.woregister);
+
+        register.setOnClickListener(v -> startActivity(new Intent(this, RegActivity.class)));
+        login.setOnClickListener(v -> startActivity(new Intent(this, LogActivity.class)));
+    }
+}
